d5601bd85ac2acd205bc1bb2ee91a003e4ca76b5
Matija Djuricic <98923192+MatijaDjuricic@users.noreply.github.com>
2023-06-25 12:12:45 +0200

    projects

--- MrEpiko/js/JQuery.js
+++ MrEpiko/js/JQuery.js
@@ -1,4 +1,7 @@
 jQuery(document).ready(function ($) {
+    setTimeout(function () {
+        $(".loader-wrapper").fadeOut();
+    }, 1000);
     $("#hamburger-btn").click(function () {
         if($(this).hasClass('opened')) {
             $("nav ul").slideUp();
